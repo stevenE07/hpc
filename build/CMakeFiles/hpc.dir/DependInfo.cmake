77fb6a7d5f398145377c33510f241666df95109a
lhost07 <stevenestramil@gmail.com>
2024-07-18 15:44:28 -0300

    mpi hola mundo

---
+++ build/CMakeFiles/hpc.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/ens/home01/e/edison.estramil/hpc/main.cpp" "CMakeFiles/hpc.dir/main.cpp.o" "gcc" "CMakeFiles/hpc.dir/main.cpp.o.d"
+  "/ens/home01/e/edison.estramil/hpc/src/Calle.cpp" "CMakeFiles/hpc.dir/src/Calle.cpp.o" "gcc" "CMakeFiles/hpc.dir/src/Calle.cpp.o.d"
+  "/ens/home01/e/edison.estramil/hpc/src/CargarGrafo.cpp" "CMakeFiles/hpc.dir/src/CargarGrafo.cpp.o" "gcc" "CMakeFiles/hpc.dir/src/CargarGrafo.cpp.o.d"
+  "/ens/home01/e/edison.estramil/hpc/src/Grafo.cpp" "CMakeFiles/hpc.dir/src/Grafo.cpp.o" "gcc" "CMakeFiles/hpc.dir/src/Grafo.cpp.o.d"
+  "/ens/home01/e/edison.estramil/hpc/src/Vehiculo.cpp" "CMakeFiles/hpc.dir/src/Vehiculo.cpp.o" "gcc" "CMakeFiles/hpc.dir/src/Vehiculo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
